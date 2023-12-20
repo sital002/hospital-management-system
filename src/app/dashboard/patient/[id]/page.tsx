b3c96a649f721ad446e71d799835d8ee3c7787d4
Saroj Aryal <114373481+saroj018@users.noreply.github.com>
2023-12-20 16:10:32 +0545

    update:add patient view page (#21)

--- src/app/dashboard/patient/[id]/page.tsx
+++ src/app/dashboard/patient/[id]/page.tsx
@@ -1,4 +1,6 @@
+import Button from "@/components/common/Button";
 import PatientDetailCard from "@/components/patientDetails/PatientDetailCard";
+import LabRecords from "@/components/patientDetails/table/LabRecords";
 import PatientReportDetails from "@/components/patientDetails/table/VitalsTable";
 import { PatientType } from "@/database/modals/PatientModel";
 
@@ -21,11 +23,17 @@ export default async function Page({ params }: { params: { id: string } }) {
   if (!patient) return null;
   return (
     <>
-    <h1 className="ml-16 text-xl font-semibold">{`${patient.name}'s Profile`}</h1>
-    <div className="flex  gap-4 ">
-    <PatientDetailCard patient={patient}/>
-    <PatientReportDetails/>
-    </div>
+      <div className="m-6 flex flex-col gap-10 rounded-lg  lg:flex-row ">
+        <PatientDetailCard patient={patient} />
+        <div className="h-fit w-full max-w-[750px]  bg-neutral-50 p-5 shadow-md">
+          <div className="flex gap-2">
+            <Button>Prescription</Button>
+            <Button>Vitals</Button>
+            <Button>Lab Records</Button>
+          </div>
+          <LabRecords />
+        </div>
+      </div>
     </>
   );
 }
