e6759878e3c705e8b2062b696a70dcd21d9c9d66
Saroj Aryal <114373481+saroj018@users.noreply.github.com>
2023-12-20 16:10:32 +0545

    update:add patient view page (#21)

--- src/components/patientDetails/PatientDetailCard.tsx
+++ src/components/patientDetails/PatientDetailCard.tsx
@@ -1,39 +1,23 @@
-import React from 'react'
-import { PatientType } from '@/database/modals/PatientModel'
+import React from "react"
+import { PatientType } from "@/database/modals/PatientModel"
 
 const PatientDetailCard = ({patient}:{patient:PatientType}) => {
   return (
-    <div className=" m-10">
-      <div className="min-w-[500px] w-full bg-white p-6 rounded-lg shadow-md">
-        <h2 className="text-2xl font-semibold mb-4">Patient Details</h2>
-
-        <div className="mb-4">
-          <label  className="block text-gray-600 text-sm  mb-2">Name</label>
-          <p  className="text-gray-800 text-lg">{patient.name}</p>
-        </div>
-
-        <div className="mb-4">
-          <label  className="block text-gray-600 text-sm  mb-2">Age</label>
-          <p  className="text-gray-800 text-lg">45 Years</p>
-        </div>
-
-        <div className="mb-4">
-          <label  className="block text-gray-600 text-sm  mb-2">Gender</label>
-          <p className="text-gray-800 text-lg">{patient.gender}</p>
-        </div>
-
-        <div className="mb-4">
-          <label  className="block text-gray-600 text-sm  mb-2">Date of Birth</label>
-          <p  className="text-gray-800 text-lg">demo</p>
-        </div>
-
-        <div className="mb-4">
-          <label  className="block text-gray-600 text-sm  mb-2">Address</label>
-          <p className="text-gray-800 text-lg">{patient.address}</p>
-        </div>
-
+    <div className="bg-neutral-50 rounded-lg overflow-hidden shadow-md max-w-[570px] w-full">
+    <div className="p-4">
+      <h2 className="text-lg font-bold text-gray-800 mb-6">{patient.name} Profile</h2>
+      <img src="https://th.bing.com/th/id/OIP.EYHt8f5tqkTU5l6szP5XbQHaE8?rs=1&pid=ImgDetMain" alt="" className="w-20 h-20 rounded-full mx-auto mb-4" />
+      <div className="text-gray-700 flex flex-col gap-10 p-6">
+        <p><span className="font-bold mr-16">Full Name:</span> {patient.name}</p>
+        <p><span className="font-bold mr-16">Mobile:</span> {patient.phone}</p>
+        <p><span className="font-bold mr-16">Address:</span>{patient.address}</p>
+        {/* <p><span className="font-bold mr-16">Date Of Birth:</span>{patient.dob}</p> */}
+        <p><span className="font-bold mr-16">Patient Type:</span>{patient.patientType}</p>
+        <p><span className="font-bold mr-16">Admit Type:</span>{patient.admitType}</p>
+        {/* <p><span className="font-bold mr-16">Data Recorded:</span>{patient.updatedAt}</p> */}
       </div>
     </div>
+  </div>
   )
 }
 
