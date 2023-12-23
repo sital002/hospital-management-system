7c1a776f6ff7c2e53423d3ed0224c21960b164d7
saroj <sarojaryal2003@gmail.com>
2023-12-23 18:59:36 +0545

    update:fix error on dropdown

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,41 @@
+import { getUserDetails } from "@/utils/Auth";
 import Link from "next/link";
-import React from "react";
+import { MdDashboard } from "react-icons/md";
 
-export default function Navbar() {
+export default async function Navbar() {
+  const user = await getUserDetails();
+
   return (
-    <nav className="flex items-center justify-center gap-3 bg-white py-3 text-black">
-      <Link href={"/"} legacyBehavior>
-        <a>Home</a>
-      </Link>
-      <Link href={"/signup"} legacyBehavior>
-        <a>Sign Up</a>
-      </Link>
-      <Link href={"/signin"} legacyBehavior>
-        <a>Sign In</a>
-      </Link>
-      <Link href={"/dashboard"} legacyBehavior>
-        <a>Dashboard</a>
-      </Link>
+    <nav className=" relative w-full bg-[#092635] p-2  ">
+      <div className="relative flex items-center justify-between px-3 py-3">
+        <div className="flex cursor-pointer items-center  gap-2 pt-4 text-xl font-semibold text-white">
+          <MdDashboard />
+          <span>Hospital MS</span>
+        </div>
+
+        {user ? (
+          <div className=" flex cursor-pointer gap-3">
+            <img
+              className="h-[50px] w-[50px] rounded-full border-2 border-white p-[1px]"
+              src="https://th.bing.com/th/id/OIP.HLuY60jzx5puuKjbqmWRRwHaEK?rs=1&pid=ImgDetMain"
+              alt=""
+            />
+            <div className="text-white">
+              <p className="text-lg">Sital Adhikari</p>
+              <p className="text-sm">Admin</p>
+            </div>
+          </div>
+        ) : (
+          <div>
+            <Link
+              href="/auth/admin"
+              className="rounded-lg bg-[#00bfa6] px-4 py-2 text-white"
+            >
+              Sign In
+            </Link>
+          </div>
+        )}
+      </div>
     </nav>
   );
 }
