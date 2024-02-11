7d1b077db4cf4d381b121c2ca8621dfadd94d4f5
sital002 <93700622+sital002@users.noreply.github.com>
2024-02-11 11:46:03 +0545

    update: dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,9 +5,9 @@ import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import { PatientTable } from "@/components/data-table";
 import Stats from "@/components/stats";
-import {getAllUsers as getDoctors} from '@/app/dashboard/doctor/page'
+import { getAllUsers as getDoctors } from "@/app/dashboard/doctor/page";
 
-export const getAllUsers = async () => {
+const getAllUsers = async () => {
   const authToken = cookies().get("auth_token")?.value;
   try {
     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/patient`, {
@@ -24,18 +24,24 @@ export const getAllUsers = async () => {
     return [];
   }
 };
-export default async function Dashboard() {
+export default async function page() {
   const user = await getUserDetails();
   if (!user) return redirect("/auth/admin");
 
   const data = await getAllUsers();
-  const totalPatient=data.length
-  const inPatient=data.filter((item,index)=>item.patientType==='inpatient')
-  const doctor=await getDoctors()
+  const totalPatient = data.length;
+  const inPatient = data.filter(
+    (item, index) => item.patientType === "inpatient",
+  );
+  const doctor = await getDoctors();
   // console.log(data);
   return (
     <div className="px-2">
-      <Stats totalPatient={totalPatient} inPatient={inPatient.length} doctor={doctor.length} />
+      <Stats
+        totalPatient={totalPatient}
+        inPatient={inPatient.length}
+        doctor={doctor.length}
+      />
       <PatientTable users={data} />
     </div>
   );
