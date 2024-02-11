2868e2dac50d5cfdba4015d2d41fd03dd684d095
sital002 <93700622+sital002@users.noreply.github.com>
2024-02-11 13:09:50 +0545

    update signin route

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -49,7 +49,7 @@ export default async function Navbar() {
           ) : (
             <div>
               <Link
-                href="/auth/admin"
+                href="/signin"
                 className="rounded-lg bg-primary px-4 py-2 text-white "
               >
                 Sign In
--- src/components/signin.tsx
+++ src/components/signin.tsx
@@ -41,7 +41,7 @@ export default function SignIn() {
       const json = await res.json();
       if (json.success) {
         toast.success("Logged in successfully");
-        router.push(json.redirect);
+        router.replace("/dashboard");
         router.refresh();
         return;
       }
