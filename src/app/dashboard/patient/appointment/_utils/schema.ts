5f09d15daa402408e2f221627ea1abc855ae8f3d
sital002 <93700622+sital002@users.noreply.github.com>
2024-03-05 19:36:39 +0545

    update appointment form

--- src/app/dashboard/patient/appointment/_utils/schema.ts
+++ src/app/dashboard/patient/appointment/_utils/schema.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-export const FormSchema = z.object({
+export const AppointmentFormSchema = z.object({
   patientId: z.string({
     required_error: "PatientId is required",
   }),
