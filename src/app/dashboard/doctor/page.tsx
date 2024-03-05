127c91881e46c411081463e9c9378146e8760f95
sital002 <93700622+sital002@users.noreply.github.com>
2024-03-05 20:56:04 +0545

    update

--- src/app/dashboard/doctor/page.tsx
+++ src/app/dashboard/doctor/page.tsx
@@ -5,7 +5,7 @@ import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import Stats from "@/components/stats";
 
-export const getAllUsers = async () => {
+const getAllUsers = async () => {
   const authToken = cookies().get("auth_token")?.value;
   try {
     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/doctor`, {
