2666095e0a3e46fde106615292dcfed97af161ea
sital002 <93700622+sital002@users.noreply.github.com>
2024-05-05 12:24:28 +0545

    Patient status (#68)

--- src/app/dashboard/patient/appointment/_components/ViewAppointment.tsx
+++ src/app/dashboard/patient/appointment/_components/ViewAppointment.tsx
@@ -6,11 +6,11 @@ type ViewAppointmentProps = {
   appointments: string;
 };
 export function ViewAppointment({ appointments }: ViewAppointmentProps) {
-  const data:TAppointment[]=JSON.parse(appointments)
-  console.log(data)
+  const data: TAppointment[] = JSON.parse(appointments);
+  // console.log(data)
   return (
     <div>
-      <h1 className="text-2xl font-bold my2">Appointments</h1>
+      <h1 className="my2 text-2xl font-bold">Appointments</h1>
       <div>
         {data.map((appointment) => {
           return (
